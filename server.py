473ffafd8266bbe8837fad11006289b376d6b80c
gustavocern <109984884+gustavocern@users.noreply.github.com>
2023-09-29 16:32:44 +0000

    cambios nuevos

--- server.py
+++ server.py
@@ -1,21 +1,34 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse
-port = 8080
+import crud_alumnos
+import json
+
+crud_alumnos = crud_alumnos.crud_alumnos()
+port = 8080 
 
 class miServer(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path=="/":
             self.path = "index.html"
+            
             return SimpleHTTPRequestHandler.do_GET(self)
+        if self.path=="/alumnos":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(crud_alumnos.consultar_alumnos()).encode('utf-8'))
+            
     def do_POST(self):
         longitud = int(self.headers["Content-Length"])
         datos= self.rfile.read(longitud)
         datos = datos.decode()
         datos = parse.unquote(datos)
+        datos = json.loads(datos)
+        if self.path=="/alumnos":
+            resp = {"msg": crud_alumnos.administrar(datos)}
         
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(datos.encode())
+        self.wfile.write(json.dumps(resp).encode())
         
 print("Ejecuntando server en puerto ", port)
 server = HTTPServer(("localhost", port), miServer)
